72e7952a9725bed6bc82939ccc5db19299b29496
mayankvanjani <mv1506@nyu.edu>
2018-11-23 17:27:47 -0500

    Derivation and Cleanup

--- Lab_4/Lab_4.m
+++ Lab_4/Lab_4.m
@@ -8,9 +8,26 @@ step = @(n, t) n >= t;
 delta = @(n,t) n == t;
 ramp = @(n,t) (n-t).*(n >= t);
 
+
 %% 1: Difference Equation
 % H(z) = Y/X
 % Result takes the form of a damped sinusoid
+
+%{
+        Derivation of Impulse Response
+
+H(z) = (1 - 2.5z^(-1)) / (1 - z^(-1) + 0.7z^(-2))
+X(n - m) <=> z^(-m)X(z)
+y(s) = x(s) = 0 for s < 0
+
+y(0) = x(0) - 2.5x(-1) + y(-1) -0.7y(-2) = x(0)
+y(1) = x(1) - 2.5x(0) + y(0) -0.7y(-1) = x(1) - 2.5x(0) + y(0)
+y(2) = x(2) - 2.5x(1) + y(1) -0.7y(0)
+--> y(n) = x(n) - 2.5(n-1) + y(n-1) - 0.7y(n-2) for n >= 2
+Above steps in myDiffeq function
+
+%}
+
 n = 0:30;
 impulse = delta(n,0);
 
@@ -36,6 +53,7 @@ hold off;
 xlim([-5 30]);
 legend("Difference Equation", "Filter");
 title("Difference Equation Impulse Response");
+
 
 %% 2: Partial Franction Expansion
 % Using MATLAB Residue Command
@@ -80,6 +98,7 @@ a1 = angle(C1); a2 = angle(C2);
 r1 = abs(p1); r2 = abs(p2);
 B1 = angle(p1); B2 = angle(p2);
 
+%{
 if (R2 == R1) 
     disp("R2 == R1 True");
 end
@@ -92,6 +111,7 @@ end
 if (B2 == -1*B1) 
     disp("B2 == -B1 True");
 end
+%}
 
 % Analytic Expression Variables and Calculation
 % h(n) = 2R1 * r1^n * cos(B1n+a1) * u(n)
@@ -131,6 +151,7 @@ xlim([-5 30]);
 legend("Discrete", "Continuous");
 title("Continuous Signal with Small Increments Overlay");
 
+
 %% 4: Z-Plane
 % Z-Plane/Transform shows stability
 % Poles inside unit circle = Stable
@@ -144,6 +165,7 @@ title("Pole Zero Diagram");
 
 %% 5: New Equation
 % Complex Conjugates and Stable = Decaying Sinusoid
+% Copied steps from previous equation except an extra component/term
 
 % 1
 step = @(n, t) n >= t;
@@ -207,10 +229,11 @@ r1 = abs(p1); r2 = abs(p2); r3 = abs(p3);
 B1 = angle(p1); B2 = angle(p2); B3 = angle(p3);
 
 % h(n) = 3R1 * r1^n * cos(B1n+a1) * u(n)
-A = 2 * R1;
+A = R1 * 2;
 w0 = B1;
 theta0 = a1;
-h_analytic = (A * r1.^n .* cos(w0*n + theta0) .* step(n,0)) + (R3 * p3.^n .* step(n,0));
+h_analytic = (A * r1.^n .* cos(w0*n + theta0) .* step(n,0)) + (R3 * r3.^n .* step(n,0));
+% (R3 * r3.^n .* step(n,0)) Extra component
 
 figure(7); clf;
 % Enable Fullscreen Figure (small graphs otherwise)
@@ -232,7 +255,7 @@ xlim([-5 30]);
 title("Difference Between Analytic and Partial (should be just error)");
 
 subplot(3,1,3);
-t = 0:0.01:30;
+t = 0:0.01:29;
 h_analytic_cont = A * r1.^t .* cos(w0*t + theta0) .* step(t,0);
 stem(1:len_h,h_analytic,'b:', "Linewidth", 1.25);
 hold on;
